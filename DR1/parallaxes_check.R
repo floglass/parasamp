53b5e4ebb141c96de333e573fc7f4ecd6f82556e
floglass <glass.florian@gmail.com>
2017-02-02 13:51:39 +0100

    Code cleanup

--- DR1/parallaxes_check.R
+++ DR1/parallaxes_check.R
@@ -7,26 +7,11 @@
 
 library(leiv)  # computes the slope and intercept of a dataset using error-in-variables
 library(ggplot2)
+library(grid)
 
 theme_set(theme_bw(14))  # define the "black and white" ggplot2 theme with font.size of 14 for the whole script
 
-###########################
-### Function definition ###
-
-#' Remove negative parallaxes from data.frame
-#' 
-#' @param to.subset The data.frame to subset
-#' @return subsetted data.frame
-remove.negatives <- function(to.subset){
-  if (length(to.subset) > 1){
-    to.subset <- to.subset[[1]]
-  }
-  neg.para <- which(to.subset < 0.)
-  to.subset.pos <- to.subset[-c(neg.para),1]
-  return(to.subset.pos)
-}
-
-### parameters and data handling
+### parameters and data loading
 iterations <- 2
 DeRidder.data <- read.csv(file="KeplerGiants_inputForGeert2.txt", header=TRUE, sep=" ")
 TGAS.parallaxes <- DeRidder.data[2]
@@ -47,48 +32,35 @@ p <-ggplot(Odata, aes(x=Odata[[1]], y=Odata[[2]])) +
   geom_errorbarh(aes(xmin = Odata[[1]]-TGAS.errors, xmax = Odata[[1]]+TGAS.errors), col='gray') + geom_point() + 
   xlab("TGAS parallaxes [mas]") + ylab("seismic parallaxes [mas]") + ggtitle("Original data") + theme(plot.title=element_text(hjust=0.5))
 print(p)
-############
+#####################
 
-### main loop
+### Creation of synthetic data:
 synthetic.seismic.parallaxes <- TGAS.parallaxes
 names(synthetic.seismic.parallaxes) <- c("synthetic seismic parallaxes")
 
-#noised <- vector('numeric', nrow(synthetic.seismic.parallaxes))
 synthetic.noised <- list()
 TGAS.noised <- list()
 
-### pick "iterations" element on each sample point for computation efficiency.
+### pick "iterations" element on each sample point for computation efficiency. Reshape data later.
 row.number <- as.numeric(rownames(synthetic.seismic.parallaxes))
 for (i in row.number){
   synthetic.noised[[i]] <- rnorm(iterations, mean=synthetic.seismic.parallaxes[i,1], sd=seismic.errors[i,1])
   TGAS.noised[[i]] <- rnorm(iterations, mean=TGAS.parallaxes[i,1], sd=TGAS.errors[i,1])
 }
 
-### Need to get the 100 iterations of 939 samples, NOT 939 samples of 100 iterations as the output of the for loop
 data.frame.synthetic.noised <- data.frame(synthetic.noised)
 names(data.frame.synthetic.noised) <- row.number
 data.frame.TGAS.noised <- data.frame(TGAS.noised)
 names(data.frame.TGAS.noised) <- row.number
 
-### Create data.frame containing 100 iterations of the sample of 939 objects, also select a positive subset
 astero.synth <- data.frame(t(data.frame.synthetic.noised))
 TGAS.synth <- data.frame(t(data.frame.TGAS.noised))
 
-### Create new data.frame with only positive TGAS.synth parallaxes
-### !!! Row number might differ since it's a random process. Data.frame only accept same number of rows in all dimensions. !!!
-# for (i in seq(iterations)){
-#   if (i==1){
-#     positive.indexes <- data.frame(which(TGAS.synth[[1]]>0.))
-#   }
-#   else{
-#     positive.indexes <- cbind(positive.indexes, which(TGAS.synth[[i]]>0.))
-#   }
-# }
 #################################################
 ### Error-in-variables for all the iterations ###
 #################################################
-### leiv.function allows to buffer for data handling. We have a data.frame (synthetic data), but need to get
-### subsets of different size from that (positive data) which data.frame doesn't accept.
+### leiv.function buffers the data handling. We have a data.frame (synthetic data), but need to get
+### subsets of different sizes from that (positive data) which data.frame doesn't accept.
 leiv.function <- function(astero, TGAS){
   fit <- leiv(astero~TGAS, abs.tol=5e-6)
   return(c(fit@slope, fit@intercept))
@@ -96,7 +68,7 @@ leiv.function <- function(astero, TGAS){
 
 fit.results <- data.frame()
 fit.pos.results <- data.frame()
-cat("Processing column:\n")
+cat("Processing sample set:\n")
 for (i in seq(iterations)){
   col.name <- paste('X', i, sep='')
   cat(col.name, '\n')
@@ -108,21 +80,6 @@ for (i in seq(iterations)){
 }
 names(fit.results) <- c('slope', 'intercept')
 names(fit.pos.results) <- c('slope', 'intercept')
-
-# fit.slope <- vector('numeric', iterations)
-# fit.intercept <- vector('numeric', iterations)
-# cat("Processing column:\n")
-# for (i in seq(iterations)){
-#   col.name <- paste('X', i, sep='')
-#   cat(col.name, '\n')
-#   fit <- leiv(astero.synth[[col.name]]~TGAS.synth[[col.name]], abs.tol=1e-5)  # must specify abs.tol, since data is nearly linear (see leiv documentation)
-#   fit.slope[[i]] <- fit@slope
-#   fit.intercept[[i]] <- fit@intercept
-# }
-# 
-# fit.results <- data.frame(cbind(fit.slope, fit.intercept))
-# names(fit.results) <- c('slope', 'intercept')
-#################################################
 
 ### Whole sample
 average.slope <- mean(fit.results$slope)
@@ -147,25 +104,25 @@ error.pos.intercept <- sd.pos.intercept/sqrt(iterations)
 X1 <- data.frame(TGAS.synth[['X1']], astero.synth[['X1']])
 grob <- grobTree(textGrob(sprintf('Current sample, slope: %s, intercept: %s',round(fit.results[[1]][[1]],3), round(fit.results[[2]][[1]],3)), x=0.01,  y=0.98, hjust=0,
                           gp=gpar(col="black", fontsize=13)))
-grob2 <- grobTree(textGrob(sprintf("All samples, slope: %s+/-%s, intercept: %s+/-%s", round(average.slope,3), round(error.slope,3), round(average.intercept,3),round(error.intercept,3)), x=0.01, y=0.95, hjust=0, 
+grob2 <- grobTree(textGrob(sprintf("Average all samples, slope: %s±%s, intercept: %s±%s", round(average.slope,3), round(error.slope,3), round(average.intercept,3),round(error.intercept,3)), x=0.01, y=0.95, hjust=0, 
                            gp=gpar(col="blue", fontsize=13)))
-grob3 <- grobTree(textGrob(sprintf("All positive values, slope: %s+/-%s, intercept: %s+/-%s", round(average.pos.slope,3), round(error.pos.slope,3), round(average.pos.intercept,3), round(error.pos.intercept,3)), x=0.01, y=0.92, hjust=0,
+grob3 <- grobTree(textGrob(sprintf("Average all positive values, slope: %s±%s, intercept: %s±%s", round(average.pos.slope,3), round(error.pos.slope,3), round(average.pos.intercept,3), round(error.pos.intercept,3)), x=0.01, y=0.92, hjust=0,
                            gp=gpar(col="red", fontsize=13)))
 p2 <-ggplot(X1, aes(x=X1[[1]], y=X1[[2]])) +
-  #geom_errorbar(aes(ymin = X1[[2]]-seismic.errors, ymax = X1[[2]]+seismic.errors), col='gray') + 
+  #geom_errorbar(aes(ymin = X1[[2]]-seismic.errors, ymax = X1[[2]]+seismic.errors), col='gray') +  # should errorbars be shown ?
   #geom_errorbarh(aes(xmin = X1[[1]]-TGAS.errors, xmax = X1[[1]]+TGAS.errors), col='gray') + 
   geom_point() + 
-  geom_abline(aes(color="Current sample"), intercept=fit.results[[2]][[1]], slope=fit.results[[1]][[1]], colour='black', linetype='dashed', size=1) +
-  geom_abline(aes(color="All samples"), intercept=average.intercept, slope=average.slope, colour='blue', size=1) +  # averaged regression of the whole iteration dataset
-  geom_abline(aes(color="Positive samples"), intercept=average.pos.intercept, slope=average.pos.slope, colour='red', size=1) +
+  geom_abline(intercept=fit.results[[2]][[1]], slope=fit.results[[1]][[1]], colour='black', linetype='dashed', size=1) +
+  geom_abline(intercept=average.intercept, slope=average.slope, colour='blue', size=1) +  # averaged regression of the whole iteration dataset
+  geom_abline(intercept=average.pos.intercept, slope=average.pos.slope, colour='red', size=1) +
   geom_vline(xintercept=0, colour='black', linetype='dashed') + 
-  #geom_hline(yintercept=fit.results[[1]][[2]], colour='black') +
-  xlab("synthetic TGAS parallaxes [mas]") + ylab("synthetic seismic parallaxes [mas]") + ggtitle("Synthetic data (showing one)") + 
+  xlab("synthetic TGAS parallaxes [mas]") + ylab("synthetic seismic parallaxes [mas]") + ggtitle("Synthetic data (showing one sample)") + 
   theme(plot.title=element_text(hjust=0.5)) +
   scale_colour_manual(name = '', values=c("Current sample", "All samples", "Positive samples")) +
   annotation_custom(grob) + annotation_custom(grob2) + annotation_custom(grob3)
 print(p2)
 
-cat('Error-in-variable on sample X1:\nslope:', fit.results[[1]][[1]], 'intercept:', fit.results[[2]][[1]],'\n')
-cat('Error-in-variable on whole sample:\nslope:', average.slope,'+/-',error.slope,'intercept:',average.intercept,'+/-',error.intercept,'\n')
-cat('Error-in-variable on positive sample:\nslope:', average.pos.slope,'+/-',error.pos.slope,'intercept:',average.pos.intercept,'+/-',error.pos.intercept,'\n')
+### Terminal output
+cat('Error-in-variable on sample 1:\nslope:', fit.results[[1]][[1]], 'intercept:', fit.results[[2]][[1]],'\n')
+cat('Error-in-variable averaged on whole sample:\nslope:', average.slope,'±',error.slope,'intercept:',average.intercept,'±',error.intercept,'\n')
+cat('Error-in-variable averaged on positive samples:\nslope:', average.pos.slope,'±',error.pos.slope,'intercept:',average.pos.intercept,'±',error.pos.intercept,'\n')
